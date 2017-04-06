4b9df9c9efb66085b69841dd71d7c0dd63c3f0fa
ahreum <dogfeeter@gmail.com>
2017-04-06 15:03:23 +0900

    dfdfdf

--- src/jackpot/DTO/msgDTO.java
+++ src/jackpot/DTO/msgDTO.java
@@ -1,10 +1,20 @@
 package jackpot.DTO;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
 public class msgDTO {
 
+	private int msg_num;
 	private String emp_num;
+	private String msg_cate;
+	private String msg_send;
+	private String msg_receive;
+	private String msg_title;
+	private String msg_content;
+	private Date msg_read;
+	private Date msg_st;
+	private String
 
 	private String hiredate;
 	private Timestamp retire;
